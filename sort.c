977bc3d54a60a818fe76ab09d4c852fa8f1bebe3
Max <max.dupenois@forward.co.uk>
2015-05-26 19:07:12 +0100

    Playing with pointers and structs

--- sort.c
+++ sort.c
@@ -9,50 +9,46 @@
 #define MAX_DATA 100
 #define MAX_DATUM_LENGTH 100
 
+struct data_struct {
+  int * data_arr;
+  size_t size;
+};
+
 void print_algorithm_choices();
+void collect_data(struct data_struct * data);
 
 int main() {
-  //int arr[] = { 3, 1, 7, 10, 0, -1 };
-  //size_t size = sizeof(arr) / sizeof(arr[0]);
-  int algorithm, index  = 0;
-  size_t data_size;
-  int data[MAX_DATA];
-  char line[MAX_DATUM_LENGTH];
-  char c;
+  int algorithm = 0;
+  struct data_struct data;
+  data.data_arr = calloc(MAX_DATA, sizeof(int));
+  data.size = 0;
 
-  printf("Enter data, a blank line finishes:\n");
-  do {
-    for(index = 0; (c = getchar()) != '\n'; index++){
-      line[index] = c;
-    }
-    line[index] = '\0';
-    if(line[0] != '\0'){
-      data[data_size] = atoi(line);
-      data_size++;
-    }
-  } while(line[0] != '\0');
+  collect_data(&data);
+  printf("Unsorted:\n-------\n\n");
+
+  print_arr(data.data_arr, data.size);
 
   print_algorithm_choices();
   scanf("%i", &algorithm);
 
   switch(algorithm) {
     case 1:
-      bubble_sort(data, data_size);
+      bubble_sort(data.data_arr, data.size);
       break;
     case 2:
-      merge_sort(data, data_size);
+      merge_sort(data.data_arr, data.size);
       break;
     case 3:
-      insertion_sort(data, data_size);
+      insertion_sort(data.data_arr, data.size);
       break;
     case 4:
-      selection_sort(data, data_size);
+      selection_sort(data.data_arr, data.size);
       break;
     default:
       printf("Unrecognised algorithm choice\n");
       return 1;
   }
-  print_arr(data, data_size);
+  print_arr(data.data_arr, data.size);
   return 0;
 }
 
@@ -63,4 +59,22 @@ void print_algorithm_choices(){
   printf("2. Merge Sort.\n");
   printf("3. Insertion Sort.\n");
   printf("4. Selection Sort.\n");
+}
+
+void collect_data(struct data_struct * data){
+  int index = 0;
+  char c;
+  char line[MAX_DATUM_LENGTH];
+
+  printf("Enter data, a blank line finishes:\n");
+  do {
+    for(index = 0; (c = getchar()) != '\n'; index++){
+      line[index] = c;
+    }
+    line[index] = '\0';
+    if(line[0] != '\0'){
+      data->data_arr[data->size] = atoi(line);
+      data->size++;
+    }
+  } while(line[0] != '\0');
 }
